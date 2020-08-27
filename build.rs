640eddd27a20b9f199aca61db2de60c9b20dc79e
Yoshida Shin <wbcchsyn@gmail.com>
2020-08-27 19:30:45 +0900

    Create build functions to compile c library

---
+++ build.rs
@@ -0,0 +1,24 @@
+// Copyright 2020 Shin Yoshida
+//
+// This file is part of Mouse.
+//
+// Mouse is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License.
+//
+// Mouse is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Mouse.  If not, see <https://www.gnu.org/licenses/>.
+
+extern crate cc;
+
+fn main() {
+    cc::Build::new()
+        .cpp(false)
+        .file("c_src/signal.c")
+        .compile("mouse_signal")
+}
