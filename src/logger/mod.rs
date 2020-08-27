9c162cc850d72fb933609e9a71db3604f557e397
Yoshida Shin <wbcchsyn@gmail.com>
2020-08-27 17:50:14 +0900

    logger module documentation

--- src/logger/mod.rs
+++ src/logger/mod.rs
@@ -14,6 +14,36 @@
 // You should have received a copy of the GNU General Public License
 // along with Mouse.  If not, see <https://www.gnu.org/licenses/>.
 
+//! # Logger
+//!
+//! Logger enable to use macros defined in log crate.
+//! (See [log](https://crates.io/crates/log "log") for details.)
+//!
+//! ## Implementation
+//!
+//! Programmer should implement the followings.
+//!
+//! ### pub fn arguments
+//!
+//! `pub fn arguments(app: App<'static, 'static>) -> App<'static, 'static>`
+//!
+//! `arguments` adds the arguments for this module.
+//! type `App` is defined in crate `clap` .
+//! See [clap](https://crates.io/crates/clap "clap") for details.
+//!
+//! ### pub fn initializer
+//!
+//! `pub fn initializer(config: GlobalConfig) -> Result<impl ModuleInitializer, String>`
+//!
+//! On success, returns the implementation for ModuleInitializer, or the error message.
+//!
+//! ### stub implements ModuleInitializer
+//!
+//! The implementation for this module.
+//!
+//! `ModuleInitializer::init` should enable to use macros defined in `log` crate.
+//! See [log](https://crates.io/crates/log "log") for details.
+
 mod custom_logger;
 
 pub use custom_logger::*;
