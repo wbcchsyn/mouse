c0fbeb69766a2fef654ccdd65c05f92a11b66ae0
Yoshida Shin <wbcchsyn@gmail.com>
2021-01-30 03:38:51 +0900

    Create module 'logger::term_logger' for feature 'term_logger'

--- src/logger/term_logger.rs
+++ src/logger/term_logger.rs
@@ -13,6 +13,3 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with Mouse.  If not, see <https://www.gnu.org/licenses/>.
-
-#[cfg(feature = "term_logger")]
-mod term_logger;
