4ec5bab7144e26e0568b08a41ff1d28ca50c2437
Yoshida Shin <wbcchsyn@gmail.com>
2021-02-01 13:16:07 +0900

    Create function ResourceId::new()

--- src/data_types/resource.rs
+++ src/data_types/resource.rs
@@ -16,6 +16,8 @@
 
 //! `resource` defines struct `Resource` and relatings.
 
+use core::mem::MaybeUninit;
+
 /// The total buffer size of the `ResourceId` .
 pub const RESOURCE_ID_BUFFER_CAPACITY: usize = 118; // The total size of 'Resource' will be 128.
 
@@ -59,4 +61,56 @@ pub struct ResourceId {
     buffer: [u8; RESOURCE_ID_BUFFER_CAPACITY],
     owner_len: u8,
     asset_type_len: u8,
+}
+
+impl ResourceId {
+    /// Creates a new instance from `owner` and `asset_type` .
+    ///
+    /// # Safety
+    ///
+    /// The behavior is undefined if `owner.len() + asset_type.len()` is greater than
+    /// [`RESOURCE_ID_BUFFER_CAPACITY`] .
+    ///
+    /// [`RESOURCE_ID_BUFFER_CAPACITY`]: constant.RESOURCE_ID_BUFFER_CAPACITY.html
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use mouse::data_types::ResourceId;
+    ///
+    /// let owner = &[1,2,3];
+    /// let asset_type = "asset name".as_ref();
+    ///
+    /// let _resource_id = unsafe { ResourceId::new(owner, asset_type) };
+    /// ```
+    #[inline]
+    pub unsafe fn new(owner: &[u8], asset_type: &[u8]) -> Self {
+        debug_assert!(owner.len() + asset_type.len() <= RESOURCE_ID_BUFFER_CAPACITY);
+
+        let mut ret: MaybeUninit<Self> = MaybeUninit::uninit();
+
+        let to_init = &mut *ret.as_mut_ptr();
+
+        to_init.owner_len = owner.len() as u8;
+        to_init.asset_type_len = asset_type.len() as u8;
+
+        let ptr = to_init.buffer.as_mut_ptr();
+        ptr.copy_from_nonoverlapping(owner.as_ptr(), owner.len());
+        let ptr = ptr.add(owner.len());
+        ptr.copy_from_nonoverlapping(asset_type.as_ptr(), asset_type.len());
+
+        ret.assume_init()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use core::mem::size_of;
+
+    #[test]
+    fn resource_id_size() {
+        // The size of 'ResourceId' should be multipiles of '8'.
+        assert_eq!(0, size_of::<ResourceId>() % 8);
+    }
 }
