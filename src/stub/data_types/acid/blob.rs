18bcf7eb1f429e5de25f69412d980105bc763436
Yoshida Shin <wbcchsyn@gmail.com>
2021-03-16 10:45:33 +0900

    Implement 'From<&[u8]>' for 'stub::data_types::Blob'

--- src/stub/data_types/acid/blob.rs
+++ src/stub/data_types/acid/blob.rs
@@ -15,7 +15,7 @@
 // along with Mouse.  If not, see <https://www.gnu.org/licenses/>.
 
 use crate::data_types::{Acid, CVec, CryptoHash, Id, Resource};
-use bsn1::DerRef;
+use bsn1::{ClassTag, Der, DerRef, PCTag};
 use core::any::TypeId;
 use std::borrow::Cow;
 use std::error::Error;
@@ -34,6 +34,22 @@ impl From<&DerRef> for Intrinsic {
     }
 }
 
+impl From<&[u8]> for Intrinsic {
+    fn from(bytes: &[u8]) -> Self {
+        let id = bsn1::Id::new(ClassTag::Application, PCTag::Primitive, 1);
+        let der = Der::new(id.as_ref(), bytes);
+        Self {
+            data: CVec::from(der.into_vec()),
+        }
+    }
+}
+
+impl Intrinsic {
+    pub fn id(&self) -> Id {
+        Id::calculate(self.data.as_ref())
+    }
+}
+
 /// `Blob` implements `Acid` , and represents binary data without no resource, no parents.
 ///
 /// This must not be orphan nor invalidate.
@@ -44,8 +60,16 @@ pub struct Blob {
 
 impl From<&DerRef> for Blob {
     fn from(der: &DerRef) -> Self {
-        let id_ = Id::calculate(der.as_ref());
         let intrinsic_ = Intrinsic::from(der);
+        let id_ = intrinsic_.id();
+        Self { id_, intrinsic_ }
+    }
+}
+
+impl From<&[u8]> for Blob {
+    fn from(bytes: &[u8]) -> Self {
+        let intrinsic_ = Intrinsic::from(bytes);
+        let id_ = intrinsic_.id();
         Self { id_, intrinsic_ }
     }
 }
