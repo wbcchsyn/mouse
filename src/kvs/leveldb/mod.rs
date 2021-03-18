42662d118e0ab304b9a202ac497244afb5ff3ade
Yoshida Shin <wbcchsyn@gmail.com>
2021-03-18 11:29:36 +0900

    Create struct 'kvs::leveldb::Environment'

--- src/kvs/leveldb/mod.rs
+++ src/kvs/leveldb/mod.rs
@@ -13,3 +13,7 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with Mouse.  If not, see <https://www.gnu.org/licenses/>.
+
+/// `Environment` implements `ModuleEnvironment` for this module.
+#[derive(Default)]
+pub struct Environment {}
