71a597cd9f189a19168863f912544a940320678e
Yoshida Shin <wbcchsyn@gmail.com>
2021-06-15 16:31:51 +0900

    Create function 'rdb::sqlite3::main_chain::push()'

--- src/rdb/sqlite3/main_chain.rs
+++ src/rdb/sqlite3/main_chain.rs
@@ -15,6 +15,7 @@
 // along with Mouse.  If not, see <https://www.gnu.org/licenses/>.
 
 use super::{Error, Master, Sqlite3Session};
+use crate::data_types::ChainIndex;
 
 /// Make sure to create table "main_chain".
 ///
@@ -35,10 +36,67 @@ where
     Ok(())
 }
 
+/// Insert `chain_index` into RDB table "main_chain".
+///
+/// # Warnings
+///
+/// This method does not sanitize at all except for the table constraint.
+/// (i.e. The height and the id of the `chain_index` is unique in "main_chain" if this method
+/// success.)
+pub fn push<S>(chain_index: &ChainIndex, session: &mut S) -> Result<(), Error>
+where
+    S: Master,
+{
+    const SQL: &'static str = r#"INSERT INTO main_chain (height, id) VALUES (?1, ?2)"#;
+    let session = Sqlite3Session::as_sqlite3_session(session);
+
+    let stmt = session.con.stmt(SQL)?;
+    stmt.bind_int(1, chain_index.height())?;
+    stmt.bind_blob(2, chain_index.id().as_ref())?;
+    stmt.step()?;
+
+    Ok(())
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::data_types::{BlockHeight, CryptoHash, Id};
     use crate::rdb::sqlite3::{master, Environment};
+
+    const CHAIN_LEN: usize = 10;
+    const MAX_CHAIN_HEIGHT: BlockHeight = 10;
+
+    fn ids() -> Vec<Id> {
+        let mut ret = Vec::with_capacity(CHAIN_LEN);
+        let mut id = Id::zeroed();
+
+        for i in 1..=CHAIN_LEN {
+            id[0] = i as u8;
+            ret.push(id);
+        }
+
+        ret
+    }
+
+    fn main_chain() -> Vec<ChainIndex> {
+        let mut ret = Vec::with_capacity(CHAIN_LEN);
+        for (i, id) in ids().iter().enumerate() {
+            let chain_index = ChainIndex::new((i + 1) as BlockHeight, &id);
+            ret.push(chain_index);
+        }
+        ret
+    }
+
+    fn empty_table() -> Environment {
+        let env = Environment::default();
+        {
+            let mut session = master(&env);
+            let session = Sqlite3Session::as_sqlite3_session(&mut session);
+            create_table(session).unwrap();
+        }
+        env
+    }
 
     #[test]
     fn create_table_() {
@@ -48,5 +106,20 @@ mod tests {
 
         assert_eq!(true, create_table(session).is_ok());
         assert_eq!(true, create_table(session).is_ok());
+    }
+
+    #[test]
+    fn push_() {
+        let env = empty_table();
+        let mut session = master(&env);
+
+        for c in main_chain() {
+            assert_eq!(true, push(&c, &mut session).is_ok());
+            assert_eq!(false, push(&c, &mut session).is_ok());
+        }
+
+        for c in main_chain() {
+            assert_eq!(false, push(&c, &mut session).is_ok());
+        }
     }
 }
