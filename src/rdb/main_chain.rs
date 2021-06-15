a2ebae84841d18e6d9a6d11845ca272dae04ca60
Yoshida Shin <wbcchsyn@gmail.com>
2021-06-15 16:42:34 +0900

    Create function 'rdb::main_chain::push()'

--- src/rdb/main_chain.rs
+++ src/rdb/main_chain.rs
@@ -24,3 +24,27 @@
 //!
 //! [`ChainIndex`]: crate::data_types::ChainIndex
 //! [`Id`]: crate::data_types::Id
+
+use super::{sqlite3, Master};
+use crate::data_types::ChainIndex;
+use std::error::Error;
+
+/// Insert `chain_index` into RDB table "main_chain".
+///
+/// This function execute like the following SQL.
+/// (It depends on the implementation. The real SQL may be different.)
+///
+/// INSERT INTO main_chain(height, id) VALUES (`chain_index.height()`, `chain_index.id()`)
+///
+/// # Warnings
+///
+/// This method does not sanitize at all except for the table constraint.
+/// (i.e. The height and the id of the `chain_index` is unique in "main_chain" if this method
+/// success.)
+pub fn push<S>(chain_index: &ChainIndex, session: &mut S) -> Result<(), Box<dyn Error>>
+where
+    S: Master,
+{
+    sqlite3::main_chain::push(chain_index, session)?;
+    Ok(())
+}
