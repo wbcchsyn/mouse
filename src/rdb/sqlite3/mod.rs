551babc222c55856613042cfd60f68740c4ed8f9
Yoshida Shin <wbcchsyn@gmail.com>
2021-06-16 04:46:33 +0900

    Create module 'rdb::sqlite3::acids'

--- src/rdb/sqlite3/mod.rs
+++ src/rdb/sqlite3/mod.rs
@@ -14,6 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Mouse.  If not, see <https://www.gnu.org/licenses/>.
 
+pub mod acids;
 mod connection;
 mod error;
 pub mod main_chain;
